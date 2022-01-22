a1bfc6ed6928828e95b9d099898ffb89b7cc7758
eklipsorz <eklipsorz@protonmail.ch>
2022-01-23 05:25:58 +0800

    feat: add edit and create page rendering

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,10 +6,38 @@ const db = require('../../models')
 const Todo = db.Todo
 
 
+
+// render create page 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+
+
+
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   Todo.findByPk(id)
     .then((todo) => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
+// render edit page
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  Todo.findByPk(id)
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+
+// update todo
+// create todo
+
+// delete todo
+
+// router.delete('/:id', (req, res) => {
+
+// })
 exports = module.exports = router
